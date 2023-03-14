b414442d41acd016e2119c1180a52e4e8e74e447
Gonzalo-Quiroga <gonzaloquiroga488@gmail.com>
2023-03-13 23:42:58 -0300

    Seccion de proyectos, en desarrollo

--- main.js
+++ main.js
@@ -15,3 +15,24 @@ window.onscroll = () => {
     menu.classList.remove("bx-x");
     navlist.classList.remove("open");
 }
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_hy6alyv';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Enviar Mensaje';
+      alert('Enviado!');
+    }, (err) => {
+      btn.value = 'Enviar Mensaje';
+      alert(JSON.stringify(err));
+    });
+});
